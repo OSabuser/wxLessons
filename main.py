a45681d8db628e4f38d4adbb30b965f700571610
Dmitry Akimov <107173613+OSabuser@users.noreply.github.com>
2024-01-18 12:25:50 +0300

    # wxPython Урок 1 - текстовый редактор

---
+++ main.py
@@ -0,0 +1,63 @@
+import os.path
+
+import wx
+
+
+class TextEditorFrame(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(480,272))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar()
+
+        # Setting up the menu
+        file_menu = wx.Menu()
+        menuOpen = file_menu.Append(wx.ID_OPEN, "&Open File", "Открыть файл")
+        file_menu.AppendSeparator()
+        menuAbout = file_menu.Append(wx.ID_ABOUT, "&About", "О программе")
+        file_menu.AppendSeparator()
+        menuExit = file_menu.Append(wx.ID_EXIT, "&Exit", "Выход из программы")
+
+        properties_menu = wx.Menu()
+        menuProperties = properties_menu.Append(wx.ID_PROPERTIES, "&Config", "Конфигурация")
+
+
+
+        # Create the menu bar
+        menu_bar = wx.MenuBar()
+        menu_bar.Append(file_menu, "&File")
+        menu_bar.Append(properties_menu, "&Properties")
+        self.SetMenuBar(menu_bar)
+
+        # Set events.
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+
+
+        self.Show(True)
+
+    def OnAbout(self, e):
+        dlg = wx.MessageDialog(self, "A small TE", "Title")
+        dlg.ShowModal()
+        dlg.Destroy()
+
+    def OnExit(self, e):
+        self.Close(True)
+
+    def OnOpen(self, e):
+        # Открытие файла
+        self.dir_name =""
+        dlg = wx.FileDialog(self, "Выберите файл: ", self.dir_name, "", "*.*", wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.file_name = dlg.GetFilename()
+            self.dir_name = dlg.GetDirectory()
+            f = open(os.path.join(self.dir_name, self.file_name), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
+
+
+app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
+myFrame = TextEditorFrame(None, "Текстовый редактор")
+app.MainLoop()
