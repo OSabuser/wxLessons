a768f83be3f75f3502670e9cc32421e031acc2fc
Dmitry Akimov <107173613+OSabuser@users.noreply.github.com>
2024-01-18 14:18:59 +0300

    # wxPython Урок 3 - Виджеты

--- main.py
+++ main.py
@@ -3,78 +3,62 @@ import os.path
 import wx
 
 
-class TextEditorFrame(wx.Frame):
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(480, 272))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar()
+class EditorPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        self.quote = wx.StaticText(self, label="Ваша ставка :", pos=(20, 30))
 
-        # Setting up the menu
-        file_menu = wx.Menu()
-        menuOpen = file_menu.Append(wx.ID_OPEN, "&Open File", "Открыть файл")
-        file_menu.AppendSeparator()
-        menuAbout = file_menu.Append(wx.ID_ABOUT, "&About", "О программе")
-        file_menu.AppendSeparator()
-        menuExit = file_menu.Append(wx.ID_EXIT, "&Exit", "Выход из программы")
+        self.logger = wx.TextCtrl(self, pos=(300, 20), size=(200, 300), style=wx.TE_MULTILINE | wx.TE_READONLY)
 
-        properties_menu = wx.Menu()
-        menuProperties = properties_menu.Append(wx.ID_PROPERTIES, "&Config", "Конфигурация")
+        self.button = wx.Button(self, label="Save", pos=(200, 325))
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
 
-        # Create the menu bar
-        menu_bar = wx.MenuBar()
-        menu_bar.Append(file_menu, "&File")
-        menu_bar.Append(properties_menu, "&Properties")
-        self.SetMenuBar(menu_bar)
+        self.lblname = wx.StaticText(self, label="Your name :", pos=(20, 60))
+        self.editname = wx.TextCtrl(self, value="Enter here your name", pos=(150, 60), size=(140, -1))
+        self.Bind(wx.EVT_TEXT, self.EvtText, self.editname)
+        self.Bind(wx.EVT_CHAR, self.EvtChar, self.editname)
 
-        # Set events.
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        self.sampleList = ['friends', 'advertising', 'web search', 'Yellow Pages']
+        self.lblhear = wx.StaticText(self, label="How did you hear from us ?", pos=(20, 90))
+        self.edithear = wx.ComboBox(self, pos=(150, 90), size=(95, -1), choices=self.sampleList, style=wx.CB_DROPDOWN)
+        self.Bind(wx.EVT_COMBOBOX, self.EvtComboBox, self.edithear)
+        self.Bind(wx.EVT_TEXT, self.EvtText, self.edithear)
 
-        self.nested_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.buttons = []
-        for i in range(0,6):
-            self.buttons.append(wx.Button(self, -1, f"Button &{i}"))
-            self.nested_sizer.Add(self.buttons[i], 1, wx.EXPAND)
+        # Checkbox
+        self.insure = wx.CheckBox(self, label="Do you want Insured Shipment ?", pos=(20, 180))
+        self.Bind(wx.EVT_CHECKBOX, self.EvtCheckBox, self.insure)
 
-        urgent_button = self.buttons[2]
-        self.Bind(wx.EVT_BUTTON, self.OnButton2Click, urgent_button)
+        # Radio Boxes
+        radioList = ['blue', 'red', 'yellow', 'orange', 'green', 'purple', 'navy blue', 'black', 'gray']
+        rb = wx.RadioBox(self, label="What color would you like ?", pos=(20, 210), choices=radioList, majorDimension=3,
+        style = wx.RA_SPECIFY_COLS)
 
-        self.parent_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.parent_sizer.Add(self.control, 1, wx.EXPAND)
-        self.parent_sizer.Add(self.nested_sizer, 0, wx.EXPAND)
+        self.Bind(wx.EVT_RADIOBOX, self.EvtRadioBox, rb)
 
-        self.SetSizer(self.parent_sizer)
-        self.SetAutoLayout(1)
-        self.parent_sizer.Fit(self)
+    def EvtRadioBox(self, event):
+        self.logger.AppendText('EvtRadioBox: %d\n' % event.GetInt())
 
-        self.Show(True)
+    def EvtComboBox(self, event):
+        self.logger.AppendText('EvtComboBox: %s\n' % event.GetString())
 
-    def OnButton2Click(self, e):
-        print("Button2 was pressed!")
+    def OnClick(self, event):
+        self.logger.AppendText(" Click on object with Id %d\n" % event.GetId())
 
+    def EvtText(self, event):
+        self.logger.AppendText('EvtText: %s\n' % event.GetString())
 
-    def OnAbout(self, e):
-        dlg = wx.MessageDialog(self, "A small TE", "Title")
-        dlg.ShowModal()
-        dlg.Destroy()
+    def EvtChar(self, event):
 
-    def OnExit(self, e):
-        self.Close(True)
+        self.logger.AppendText('EvtChar: %d\n' % event.GetKeyCode())
+        event.Skip()
 
-    def OnOpen(self, e):
-        # Открытие файла
-        self.dir_name = ""
-        dlg = wx.FileDialog(self, "Выберите файл: ", self.dir_name, "", "*.*", wx.FD_OPEN)
-        if dlg.ShowModal() == wx.ID_OK:
-            self.file_name = dlg.GetFilename()
-            self.dir_name = dlg.GetDirectory()
-            f = open(os.path.join(self.dir_name, self.file_name), 'r')
-            self.control.SetValue(f.read())
-            f.close()
-        dlg.Destroy()
+    def EvtCheckBox(self, event):
+        print(type(event))
+        self.logger.AppendText('EvtCheckBox: %d\n' % event.IsChecked())
 
 
 app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
-myFrame = TextEditorFrame(None, "Текстовый редактор")
+frame = wx.Frame(None)
+panel = EditorPanel(frame)
+frame.Show()
 app.MainLoop()
