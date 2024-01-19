e5af1a694f703f280afb937f0d29105b14286949
Dmitry Akimov <107173613+OSabuser@users.noreply.github.com>
2024-01-19 11:55:44 +0300

    # Работа над диалогом выбора элемента кодировки

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ class Selection(wx.Frame):
 
         btnPanel = wx.Panel(panel)
         vbox = wx.BoxSizer(wx.VERTICAL)
-        SelBtn = wx.Button(btnPanel, wx.ID_ANY, 'Select', size=(90, 30))
-        newBtn = wx.Button(btnPanel, wx.ID_ANY, 'New', size=(90, 30))
-        renBtn = wx.Button(btnPanel, wx.ID_ANY, 'Rename', size=(90, 30))
-        delBtn = wx.Button(btnPanel, wx.ID_ANY, 'Delete', size=(90, 30))
-        searchExpectedResults = wx.TextCtrl(btnPanel, -1, "", size=(90, 30))
+        SelBtn = wx.Button(btnPanel, wx.ID_ANY, 'Выбрать', size=(100, 30))
+        newBtn = wx.Button(btnPanel, wx.ID_ANY, 'Новый', size=(100, 30))
+        renBtn = wx.Button(btnPanel, wx.ID_ANY, 'Переименовать', size=(100, 30))
+        delBtn = wx.Button(btnPanel, wx.ID_ANY, 'Удалить', size=(100, 30))
+        search_expected_results = wx.TextCtrl(btnPanel, -1, "", size=(100, 30))
 
         quote = wx.StaticText(btnPanel, label="Поиск элемента", pos=(20, 30))
 
@@ -39,12 +39,12 @@ class Selection(wx.Frame):
         self.Bind(wx.EVT_BUTTON, self.OnRename, id=renBtn.GetId())
         self.Bind(wx.EVT_BUTTON, self.OnDelete, id=delBtn.GetId())
         self.Bind(wx.EVT_BUTTON, self.OnSelect, id=SelBtn.GetId())
-        self.Bind(wx.EVT_LISTBOX_DCLICK, self.OnRename)
-        self.Bind(wx.EVT_TEXT, self.OnChar, id=searchExpectedResults.GetId())
+        self.Bind(wx.EVT_LISTBOX_DCLICK, self.OnSelect)
+        self.Bind(wx.EVT_TEXT, self.OnChar, id=search_expected_results.GetId())
 
         #vbox.Add((-1, 20))
         vbox.Add(quote)
-        vbox.Add(searchExpectedResults, 0, wx.TOP)
+        vbox.Add(search_expected_results, 0, wx.LEFT)
         vbox.Add(SelBtn, 0, wx.TOP, 5)
         vbox.Add(newBtn, 0, wx.TOP, 5)
         vbox.Add(renBtn, 0, wx.TOP, 5)
@@ -57,19 +57,24 @@ class Selection(wx.Frame):
         self.Centre()
         self.Show()
 
+    def ShowErrorMessage(self, text):
+        wx.MessageBox(text, 'Ошибка',
+                      wx.OK | wx.ICON_ERROR)
+
     def OnChar(self, event):
+        #self.ShowErrorMessage("Тест!")
         typed_chars = event.GetString()
         print(typed_chars)
-        #TODO: ВЫВОД ЭЛЕМЕНТА
-        length = self.listbox.GetCount()
-        temp =[]
-        for idx in range(length):
-            temp.append(self.listbox.GetString(idx))
 
-        print(f"User_list:{self.list_items}, count:{len(self.list_items)}")
-        print(f"Listbox:{temp}, count:{length}")
+        temp = self.listbox.GetStrings()
+
+        for string in temp:
+            if typed_chars in string:
+                item_id = self.listbox.FindString(string, caseSensitive=False)
+                self.listbox.SetSelection(item_id)  # Выделение предполагаемого элемента
+                break
+
     def NewItem(self, event):
-
         text = wx.GetTextFromUser('Задайте имя нового элемента', 'Добавить новый элемент')
         if text != '':
             self.list_items.append(text)
@@ -106,9 +111,12 @@ class Selection(wx.Frame):
             deleted_element = self.listbox.GetString(selected_item)
             self.list_items.remove(deleted_element)
             self.listbox.Delete(selected_item)
+            if len(self.listbox.GetStrings()) > 0:
+                self.listbox.SetSelection(selected_item - 1)
 
 
 def main():
+    test_lst = ["TFT", "LCD", "GPI_MU", "MODB"]
     app = wx.App()
     ex = Selection(None, "ОБОЗНАЧЕНИЕ", ["TFT", "LCD", "GPI_MU", "MODB"])
     app.MainLoop()
